8485a42b3181a2057aee6d207248d0eff8c03461
kangzixiang <kangzixiang@foxmail.com>
2025-07-11 19:01:28 +0800

    修改编译脚本和gitignore

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 CURRENT_DIR=$(cd $(dirname $0); pwd)
-FILE=${CURRENT_DIR}/build
+FILE=${CURRENT_DIR}/output
 
 if [ ! -d "$FILE" ]; then
     echo "$FILE not exist"
     mkdir ${FILE}
 fi
 
-cd build
+cd ${FILE}
 
 cmake ..
 
